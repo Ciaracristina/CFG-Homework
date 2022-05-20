ee6822310f46a36abfdd393a26ec31c0f15c51dc
helenijevans <45155727+helenijevans@users.noreply.github.com>
2022-05-20 12:00:52 +0100

    Update marking.py

--- marking.py
+++ marking.py
@@ -4,7 +4,7 @@ So here is the feedback/marks which would normally be done through a PR
 
 For your function you have received 7/8, a mark of 88
 You lost one mark here unfortunately as your function started with 'test_' this means it can't be run
-This is important to know that by default any function preceded with 'test_' is used for unit tests
+This is important to know that by default any function preceded with 'test_' is used for unit tests and run automatically as a unit test
 This means you can't use that prefix for normal functions.
 
 Great work!
